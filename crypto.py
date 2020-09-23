26c4134e9aa9d0267f79974bf7ce21f963ed9e7b
Tyrest <terrifictyler8@gmail.com>
2020-09-23 13:53:09 -0700

    Finished everything other than decrypt mhkc

--- crypto.py
+++ crypto.py
@@ -48,32 +48,57 @@ def decrypt_vigenere(ciphertext, keyword):
 # Arguments: integer
 # Returns: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 def generate_private_key(n=8):
-    seq = [1]
+    W = [random.randint(1, 10)]
     for i in range(n):
-        seq.append(random.randint(sum(seq), math.inf))
-    return seq
+        W.append(random.randint(sum(W) + 1, sum(W) * 2))
+    Q = random.randint(sum(W) + 1, sum(W) * 2)
+    R = random.randint(2, Q-1)
+    while math.gcd(R, Q) != 1:
+        R = random.randint(2, Q-1)
+    return (tuple(W), Q, R)
 
 # Arguments: tuple (W, Q, R) - W a length-n tuple of integers, Q and R both integers
 # Returns: tuple B - a length-n tuple of integers
 def create_public_key(private_key):
-    pass
+    W, Q, R = private_key
+    B = []
+    for i in W:
+        B.append(R*i%Q)
+    return tuple(B)
 
 # Arguments: string, tuple (W, Q, R)
 # Returns: list of integers
-def encrypt_mhkc(plaintext, public_key):
-    pass
+def encrypt_mhkc(plaintext, B):
+    encrypted = []
+
+    for ch in plaintext:
+        A = list(bin(ord(ch))[2:])
+        A = (len(B) - len(A)) * [0] + A
+        C = []
+        for i in range(len(A)):
+            C.append(int(A[i]) * int(B[i]))
+        encrypted.append(sum(C))
+    
+    return encrypted
 
 # Arguments: list of integers, tuple B - a length-n tuple of integers
 # Returns: bytearray or str of plaintext
 def decrypt_mhkc(ciphertext, private_key):
-    pass
+    W, Q, R = private_key
+    # S is the modulo inverse of R modulo Q
+
 
 def main():
-    print(encrypt_caesar('PYTHON!*(@#&!$(!()$    . . .. . ', 55))
-    print(decrypt_caesar('SBWKRQ!*(@#&!$(!()$    . . .. .', 55))
-    print(encrypt_vigenere('ATTACKATDAWN', 'LEMON'))
-    print(decrypt_vigenere('LXFOPVEFRNHR', 'LEMON'))
-    print(generate_private_key())
+    # print(encrypt_caesar('PYTHON!*(@#&!$(!()$    . . .. . ', 55))
+    # print(decrypt_caesar('SBWKRQ!*(@#&!$(!()$    . . .. .', 55))
+    # print(encrypt_vigenere('ATTACKATDAWN', 'LEMON'))
+    # print(decrypt_vigenere('LXFOPVEFRNHR', 'LEMON'))
+    private_key = generate_private_key()
+    print(private_key)
+    public_key = create_public_key(private_key)
+    print(public_key)
+    encrypted = encrypt_mhkc("HELLO", public_key)
+    print(encrypted)
     # Testing code here
 
 if __name__ == '__main__':
